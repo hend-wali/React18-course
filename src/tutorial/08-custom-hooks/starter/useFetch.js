3472b382683d68d546f051f50592faadda5b3e07
hend <walihend001@gmail.com>
2023-11-29 13:51:56 +0100

    custom-hook

--- src/tutorial/08-custom-hooks/starter/useFetch.js
+++ src/tutorial/08-custom-hooks/starter/useFetch.js
@@ -1,23 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-  // change state value
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    // change name
     const fetchData = async () => {
       try {
         const resp = await fetch(url);
-
+        // console.log(resp);
         if (!resp.ok) {
           setIsError(true);
           setIsLoading(false);
           return;
         }
-        // change to response
+
         const response = await resp.json();
         setData(response);
       } catch (error) {
@@ -27,7 +25,6 @@ const useFetch = (url) => {
       // hide loading
       setIsLoading(false);
     };
-    // invoke fetch data
     fetchData();
   }, []);
 
--- src/tutorial/08-custom-hooks/starter/useToggle.js
+++ src/tutorial/08-custom-hooks/starter/useToggle.js
@@ -1,13 +1,10 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const useToggle = (defaultValue) => {
   const [show, setShow] = useState(defaultValue);
-
   const toggle = () => {
     setShow(!show);
   };
-
-  return { show, toggle };
+  return {show, toggle};
 };
-
 export default useToggle;
